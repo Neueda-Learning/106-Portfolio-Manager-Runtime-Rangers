83eb32264616342343ed6337a81876e4809085f4
akshatxtwri <krishi.akshat@gmail.com>
2026-08-05 08:58:23 +0000

    APi integration in market page

--- frontend/src/Pages/Market.jsx
+++ frontend/src/Pages/Market.jsx
@@ -4,17 +4,9 @@ import Navbar from "../components/layout/Navbar";
 import SearchBar from "../components/market/SearchBar";
 import TopMovers from "../components/market/TopMovers";
 import MarketTable from "../components/market/MarketTable";
-import { marketData } from "../mock/market";
 
 const Market = () => {
   const [search, setSearch] = useState("");
-
-  const filteredStocks = marketData.filter((stock) => {
-    return (
-      stock.company.toLowerCase().includes(search.toLowerCase()) ||
-      stock.symbol.toLowerCase().includes(search.toLowerCase())
-    );
-  });
 
   return (
     <div className="min-h-screen bg-[#121018] text-white">
@@ -23,17 +15,22 @@ const Market = () => {
       <div className="max-w-7xl mx-auto px-6 py-8">
         <Navbar />
 
-        <h1 className="text-3xl font-bold mt-8">Market</h1>
+        <h1 className="text-3xl font-bold mt-8">
+          Market
+        </h1>
 
         <p className="text-[#A8A4B3] mt-2">
           Explore stocks and manage your investments.
         </p>
 
-        <SearchBar search={search} setSearch={setSearch} />
+        <SearchBar
+          search={search}
+          setSearch={setSearch}
+        />
 
         <TopMovers />
 
-        <MarketTable marketData={filteredStocks} />
+        <MarketTable search={search} />
       </div>
     </div>
   );
