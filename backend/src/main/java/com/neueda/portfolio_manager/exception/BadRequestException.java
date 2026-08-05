7f4f8b6e77548f73655d133780b38c82beabac37
venktesh <venkteshpratap@gmail.com>
2026-08-05 05:56:48 +0000

    Custom exception 3 added

---
+++ backend/src/main/java/com/neueda/portfolio_manager/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.neueda.portfolio_manager.exception;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
