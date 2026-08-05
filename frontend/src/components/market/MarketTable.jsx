790473db946a6916f8a421e32376bc8356b94bbf
akshatxtwri <krishi.akshat@gmail.com>
2026-08-05 15:28:44 +0000

    updating buy logic

--- frontend/src/components/market/MarketTable.jsx
+++ frontend/src/components/market/MarketTable.jsx
@@ -2,7 +2,11 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { getMarketStocks } from "../../api/marketApi";
-import { buyStock } from "../../api/holdingApi";
+import { 
+  buyStock,
+  getHoldings,
+  updateHolding
+} from "../../api/holdingApi";
 
 const MarketTable = ({search}) => {
     const [marketData, setMarketData] = useState([]);
@@ -21,36 +25,107 @@ const MarketTable = ({search}) => {
 
 const handleBuy = async(stock)=>{
 
-    const holding = {
-         id : 1,
-        marketId: stock.id,
-        quantity: 1,
-        purchasePrice: stock.currentPrice,
-        purchaseDate: new Date()
-            .toISOString()
-            .split("T")[0]
+
+try{
+
+
+  
+
+  const response = await getHoldings();
+
+  const holdings = response.data;
+
+
+  const existingStock = holdings.find(
+    item => item.marketId === stock.id
+  );
+
+
+
+  if(existingStock){
+
+
+   
+
+    const updatedHolding = {
+
+      id : 1,
+      marketId: existingStock.marketId,
+
+      quantity: existingStock.quantity + 1,
+
+      purchasePrice: existingStock.purchasePrice,
+
+      purchaseDate: existingStock.purchaseDate
+
     };
 
 
-    try{
 
-        await buyStock(holding);
+    await updateHolding(
+      existingStock.holdingId,
+      updatedHolding
+    );
 
-        alert(
-          `${stock.symbol} bought successfully`
-        );
 
-    }
-    
-    catch(error){
+    alert(
+      `${stock.symbol} quantity increased`
+    );
 
-        console.error(
-          "Buy failed:",
-          error
-        );
 
-    }
-    
+  }
+
+  else{
+
+
+   
+
+
+    const holding = {
+
+      marketId: stock.id,
+
+      quantity: 1,
+
+      purchasePrice: stock.currentPrice,
+
+      purchaseDate:
+        new Date()
+        .toISOString()
+        .split("T")[0]
+
+    };
+
+
+
+    await buyStock(holding);
+
+
+    alert(
+      `${stock.symbol} bought successfully`
+    );
+
+  }
+
+
+
+
+  window.dispatchEvent(
+    new Event("portfolioUpdated")
+  );
+
+
+}
+
+catch(error){
+
+ console.error(
+   "Buy failed:",
+   error.response?.data || error
+ );
+
+}
+
 
 };
 
@@ -124,12 +199,34 @@ const handleBuy = async(stock)=>{
 
                 <button
  onClick={()=>handleBuy(stock)}
- className="bg-green-600 hover:bg-green-700 px-3 py-2 rounded-lg text-sm"
+ className="
+                    bg-green-500/10
+                    border border-green-500/30
+                    text-green-400
+                    hover:bg-green-500
+                    hover:text-white
+                    px-4 py-2
+                    rounded-xl
+                    text-sm
+                    font-medium
+                    transition
+                    "
 >
  Buy
 </button>
 
-                  <button className="bg-red-600 hover:bg-red-700 px-3 py-2 rounded-lg text-sm transition">
+                  <button className="
+                    bg-red-500/10
+                    border border-red-500/30
+                    text-red-400
+                    hover:bg-red-500
+                    hover:text-white
+                    px-4 py-2
+                    rounded-xl
+                    text-sm
+                    font-medium
+                    transition
+                    ">
                     Sell
                   </button>
 
