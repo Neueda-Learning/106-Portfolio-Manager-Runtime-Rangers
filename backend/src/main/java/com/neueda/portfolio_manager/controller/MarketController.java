2a3789f8073c5c97b9dc98965c2a37a8c78d6d3d
venktesh <venkteshpratap@gmail.com>
2026-08-06 05:16:49 +0000

    Cross origin ports added

--- backend/src/main/java/com/neueda/portfolio_manager/controller/MarketController.java
+++ backend/src/main/java/com/neueda/portfolio_manager/controller/MarketController.java
@@ -5,15 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.neueda.portfolio_manager.entity.Market;
 import com.neueda.portfolio_manager.service.MarketService;
@@ -29,6 +21,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
         description = "APIs for retrieving and managing market information"
 )
 @RestController
+@CrossOrigin(origins = {"http://localhost:5173","http://10.9.77.127:8085"})
 @RequestMapping("/api/market")
 public class MarketController {
     @Autowired
